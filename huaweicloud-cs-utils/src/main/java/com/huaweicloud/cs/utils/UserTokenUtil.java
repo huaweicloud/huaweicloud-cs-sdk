79fd685bf8e83bb8165e586abf2514968c429bb4
shijinkui <shijinkui@huawei.com>
2018-01-19 18:17:42 +0800

    'sf'

--- huaweicloud-cs-utils/src/main/java/com/huaweicloud/cs/utils/UserTokenUtil.java
+++ huaweicloud-cs-utils/src/main/java/com/huaweicloud/cs/utils/UserTokenUtil.java
@@ -18,9 +18,18 @@
 package com.huaweicloud.cs.utils;
 
 
-import okhttp3.*;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import javax.net.ssl.*;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.security.cert.CertificateException;
@@ -30,279 +39,201 @@ import java.util.concurrent.TimeUnit;
 
 
 public class UserTokenUtil {
+	private Logger logger = LoggerFactory.getLogger(getClass());
 
-    private static final MediaType contentType = MediaType.parse("application/json; charset=utf-8");
+	private static final MediaType contentType = MediaType.parse("application/json; charset=utf-8");
 
-    private String domainName;
-    private String userName;
-    private String password;
-    private String projectId;
-    private final OkHttpClient client;
-    private String tokenUrl = "https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens";
-    private Map<String, String> headerMap = new HashMap<String, String>() {
-        {
-            put("Content-Type", "application/json;charset=UTF-8");
-            put("Accept", "application/json");
-        }
-    };
-    private String jsonBody;
-    private String token;
-    private Request request;
-    private Response response;
+	private String tokenUrl = "https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens";
+	private Map<String, String> headerMap = new HashMap<String, String>() {
+		{
+			put("Content-Type", "application/json;charset=UTF-8");
+			put("Accept", "application/json");
+		}
+	};
 
-    /**
-     * Get IAM tokenUrl
-     *
-     * @return IAM tokenUrl
-     */
-    public String getTokenUrl() {
+	private String jsonBody;
+//	private String token;
+	private Request request;
+	private Response response;
+	private final OkHttpClient client;
 
-        return this.tokenUrl;
-    }
+	/**
+	 * Set IAM tokenUrl
+	 *
+	 * @param tokenUrl request URL for getting IAM token(e.g https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens)
+	 * @return An instance of OkHttpClient
+	 */
+	public UserTokenUtil setTokenUrl(String tokenUrl) {
+		this.tokenUrl = tokenUrl;
+		return this;
+	}
 
-    /**
-     * Set IAM tokenUrl
-     *
-     * @param tokenUrl request URL for getting IAM token(e.g https://iam.cn-north-1.myhwclouds.com/v3/auth/tokens)
-     * @return An instance of OkHttpClient
-     */
-    public UserTokenUtil setTokenUrl(String tokenUrl) {
-        this.tokenUrl = tokenUrl;
+	/**
+	 * Set IAM jsonBody
+	 *
+	 * @param jsonBody request body for getting IAM token
+	 * @return An instance of UserTokenUtil
+	 */
+	public UserTokenUtil setJsonBody(String jsonBody) {
+		this.jsonBody = jsonBody;
+		return this;
+	}
 
-        return this;
-    }
+	/**
+	 * Get IAM jsonBody
+	 *
+	 * @return IAM jsonBody
+	 */
+	public String getJsonBody() {
+		return this.jsonBody;
+	}
 
-    /**
-     * Get IAM headerMap
-     *
-     * @return UserTokenUtil
-     */
-    public Map<String, String> getHeader() {
+	/**
+	 * Get user Token
+	 *
+	 * @param domainName
+	 * @param userName
+	 * @param password
+	 * @param userName
+	 * @return IAM token
+	 */
+	public String getUserToken(String domainName, String userName, String password, String projectId) {
 
-        return this.headerMap;
-    }
+		jsonBody = "{\"auth\":{\"identity\":{\"password\":{\"user\":{\"password\":\"" + password + "\",\"domain\":{\"name\":\"" + domainName + "\"},\"name\":\"" + userName + "\"}},\"methods\":[\"password\"]},\"scope\":{\"project\":{\"id\":\"" + projectId + "\"}}}}";
 
-    /**
-     * Set IAM headerMap
-     *
-     * @param headerMap
-     * @return UserTokenUtil
-     */
-    public UserTokenUtil setHeader(Map<String, String> headerMap) {
-        this.headerMap = headerMap;
+		RequestBody requestBody = RequestBody.create(contentType, jsonBody);
+		this.request = requestBuilder(tokenUrl, headerMap).post(requestBody).build();
+		logger.info("tokenUrl: " + tokenUrl + "\nheaderMap: " + headerMap + "\njsonBody: " + jsonBody);
 
-        return this;
-    }
+		String token = "";
+		try {
+			client.newCall(this.request).execute();
+			token = responseHeadStringByName();
+			logger.info("token: " + token);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			this.close();
+		}
 
-    /**
-     * Get IAM jsonBody
-     *
-     * @return IAM jsonBody
-     */
-    public String getJsonBody() {
+		return token;
+	}
 
-        return this.jsonBody;
-    }
+	/**
+	 * build request
+	 *
+	 * @param url
+	 * @param headMap
+	 * @return
+	 */
+	private Request.Builder requestBuilder(String url, Map<String, String> headMap) {
+		Request.Builder builder = new Request.Builder().url(url);
 
-    /**
-     * Set IAM jsonBody
-     *
-     * @param jsonBody request body for getting IAM token
-     * @return An instance of UserTokenUtil
-     */
-    public UserTokenUtil setjsonBody(String jsonBody) {
-        this.jsonBody = jsonBody;
+		for (String key : headMap.keySet()) {
+			builder.addHeader(key, headMap.get(key));
+		}
 
-        return this;
-    }
+		return builder;
+	}
 
-    /**
-     * Get user Token
-     *
-     * @param domainName
-     * @param userName
-     * @param password
-     * @param userName
-     * @return IAM token
-     */
-    public String getUserToken(String domainName, String userName, String password, String projectId) {
-        this.domainName = domainName;
-        this.userName = userName;
-        this.password = password;
-        this.projectId = projectId;
+	/**
+	 * Get OkHttpClient client
+	 *
+	 * @param isSafe
+	 * @param connectTimeout
+	 * @param readTimeout
+	 */
+	public UserTokenUtil(boolean isSafe, Integer connectTimeout, Integer readTimeout) {
+		if (isSafe) {
+			this.client = createSafeHttpClient(connectTimeout, readTimeout);
+		} else {
+			this.client = createHttpClient(connectTimeout, readTimeout);
+		}
+	}
 
-        jsonBody = "{\"auth\":{\"identity\":{\"password\":{\"user\":{\"password\":\"" + this.password + "\",\"domain\":{\"name\":\"" + this.domainName + "\"},\"name\":\"" + this.userName + "\"}},\"methods\":[\"password\"]},\"scope\":{\"project\":{\"id\":\"" + this.projectId + "\"}}}}";
+	/**
+	 * Get field value of response
+	 *
+	 * @return value of field
+	 */
+	private String responseHeadStringByName() throws IOException {
+		if (this.response == null) {
+			System.out.println("ERROR: Response is NULL.");
 
-        try {
-            this.postRequest(tokenUrl, headerMap, jsonBody);
-            System.out.println("tokenUrl: " + tokenUrl + "\nheaderMap: " + headerMap + "\njsonBody: " + jsonBody);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+			return "";
+		}
+		if (!response.isSuccessful()) {
+			System.out.println("ERROR: Response Failed: " + response.toString());
 
-        try {
-            token = this.run().responseHeadStringByName("X-Subject-Token");
-            System.out.println("token: " + token);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            this.close();
-        }
+			return "";
+		}
+		if (!response.headers().names().contains("X-Subject-Token")) {
+			System.out.println("ERROR: Response does not contain [" + "X-Subject-Token" + "] :" + response.toString());
+			throw new IOException("ERROR: Response Header does not contain " + "X-Subject-Token");
+		}
 
-        return this.token;
-    }
+		return this.response.headers().get("X-Subject-Token");
+	}
 
-    /**
-     * post request
-     *
-     * @param url
-     * @param headerMap
-     * @param body
-     * @return An instance of UserTokenUtil
-     */
-    private UserTokenUtil postRequest(String url, Map<String, String> headerMap, String body) throws IOException {
-        RequestBody requestBody = RequestBody.create(contentType, body);
-        this.request = requestBuilder(url, headerMap).post(requestBody).build();
+	/**
+	 * create safe HttpClient
+	 *
+	 * @param connectTimeout
+	 * @param readTimeout
+	 * @return
+	 */
+	private OkHttpClient createSafeHttpClient(Integer connectTimeout, Integer readTimeout) {
+		X509TrustManager trustManager;
+		SSLSocketFactory sslSocketFactory;
+		try {
+			trustManager = new X509TrustManager() {
+				@Override
+				public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
+						throws CertificateException {
+				}
 
-        return this;
-    }
+				@Override
+				public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
+						throws CertificateException {
+				}
 
-    /**
-     * build request
-     *
-     * @param url
-     * @param headMap
-     * @return
-     */
-    private Request.Builder requestBuilder(String url, Map<String, String> headMap) {
-        Request.Builder builder = new Request.Builder().url(url);
+				@Override
+				public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+					return new java.security.cert.X509Certificate[]{};
+				}
+			};
+			SSLContext sslContext = SSLContext.getInstance("SSL");
+			sslContext.init(null, new TrustManager[]{trustManager}, new java.security.SecureRandom());
+			sslSocketFactory = sslContext.getSocketFactory();
+		} catch (GeneralSecurityException e) {
+			throw new RuntimeException(e);
+		}
+		return new OkHttpClient().newBuilder()
+				.connectTimeout(connectTimeout, TimeUnit.SECONDS)
+				.readTimeout(readTimeout, TimeUnit.SECONDS)
+				.sslSocketFactory(sslSocketFactory, trustManager)
+				.hostnameVerifier((hostname, session) -> true).build();
+	}
 
-        for (String key : headMap.keySet()) {
-            builder.addHeader(key, headMap.get(key));
-        }
+	/**
+	 * create HttpClient
+	 *
+	 * @param connectTimeout
+	 * @param readTimeout
+	 * @return
+	 */
+	private OkHttpClient createHttpClient(Integer connectTimeout, Integer readTimeout) {
+		return new OkHttpClient().newBuilder()
+				.connectTimeout(connectTimeout, TimeUnit.SECONDS)
+				.readTimeout(readTimeout, TimeUnit.SECONDS)
+				.build();
+	}
 
-        return builder;
-    }
-
-    /**
-     * Get OkHttpClient client
-     *
-     * @param isSafe
-     * @param connectTimeout
-     * @param readTimeout
-     */
-    public UserTokenUtil(boolean isSafe, Integer connectTimeout, Integer readTimeout) {
-        if (isSafe) {
-            this.client = createSafeHttpClient(connectTimeout, readTimeout);
-        } else {
-            this.client = createHttpClient(connectTimeout, readTimeout);
-        }
-    }
-
-    /**
-     * Execute request
-     *
-     * @return
-     * @throws IOException
-     */
-    private UserTokenUtil run() throws IOException {
-        Response response = client.newCall(this.request).execute();
-        this.response = response;
-
-        return this;
-    }
-
-    /**
-     * Get field value of response
-     *
-     * @param field
-     * @return value of field
-     */
-    private String responseHeadStringByName(String field) throws IOException {
-        if (this.response == null) {
-            System.out.println("ERROR: Response is NULL.");
-
-            return "";
-        }
-        if (!response.isSuccessful()) {
-            System.out.println("ERROR: Response Failed: " + response.toString());
-
-            return "";
-        }
-        if (!response.headers().names().contains(field)) {
-            System.out.println("ERROR: Response does not contain [" + field + "] :" + response.toString());
-            throw new IOException("ERROR: Response Header does not contain " + field);
-        }
-
-        return this.response.headers().get(field);
-    }
-
-    /**
-     * create safe HttpClient
-     *
-     * @param connectTimeout
-     * @param readTimeout
-     * @return
-     */
-    private OkHttpClient createSafeHttpClient(Integer connectTimeout, Integer readTimeout) {
-        X509TrustManager trustManager;
-        SSLSocketFactory sslSocketFactory;
-        try {
-            trustManager = new X509TrustManager() {
-                @Override
-                public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType)
-                        throws CertificateException {
-                }
-
-                @Override
-                public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType)
-                        throws CertificateException {
-                }
-
-                @Override
-                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-                    return new java.security.cert.X509Certificate[]{};
-                }
-            };
-            SSLContext sslContext = SSLContext.getInstance("SSL");
-            sslContext.init(null, new TrustManager[]{trustManager}, new java.security.SecureRandom());
-            sslSocketFactory = sslContext.getSocketFactory();
-        } catch (GeneralSecurityException e) {
-            throw new RuntimeException(e);
-        }
-        OkHttpClient okHttpClient = new OkHttpClient().newBuilder()
-                .connectTimeout(connectTimeout, TimeUnit.SECONDS)
-                .readTimeout(readTimeout, TimeUnit.SECONDS)
-                .sslSocketFactory(sslSocketFactory, trustManager)
-                .hostnameVerifier(new HostnameVerifier() {
-                    @Override
-                    public boolean verify(String hostname, SSLSession session) {
-                        return true;
-                    }
-                }).build();
-        return okHttpClient;
-    }
-
-    /**
-     * create HttpClient
-     *
-     * @param connectTimeout
-     * @param readTimeout
-     * @return
-     */
-    private OkHttpClient createHttpClient(Integer connectTimeout, Integer readTimeout) {
-        return new OkHttpClient().newBuilder()
-                .connectTimeout(connectTimeout, TimeUnit.SECONDS)
-                .readTimeout(readTimeout, TimeUnit.SECONDS)
-                .build();
-    }
-
-    /**
-     * close response
-     */
-    private void close() {
-        this.response.close();
-    }
-
+	/**
+	 * close response
+	 */
+	private void close() {
+		this.response.close();
+	}
 }
 
