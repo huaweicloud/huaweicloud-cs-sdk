da1e035576e7865092d3b0e21047ee3aaf07c131
fengyelei <fengyelei@huawei.com>
2018-02-08 17:36:54 +0800

    add file

---
+++ huaweicloud-cs-java-sdk/src/main/java/com/huaweicloud/cs/java/v1/model/UpdateJobTemplateRequest.java
@@ -0,0 +1,166 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.huaweicloud.cs.java.v1.model;
+
+import java.util.Objects;
+import com.google.gson.TypeAdapter;
+import com.google.gson.annotations.JsonAdapter;
+import com.google.gson.annotations.SerializedName;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import java.io.IOException;
+
+/**
+ * UpdateJobTemplateRequest
+ */
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.JavaClientCodegen", date = "2018-02-08T17:28:20.453+08:00")
+public class UpdateJobTemplateRequest {
+  @SerializedName("id")
+  private Long id = null;
+
+  @SerializedName("name")
+  private String name = null;
+
+  @SerializedName("desc")
+  private String desc = null;
+
+  @SerializedName("sql_body")
+  private String sqlBody = null;
+
+  public UpdateJobTemplateRequest id(Long id) {
+    this.id = id;
+    return this;
+  }
+
+   /**
+   * 模板ID
+   * @return id
+  **/
+  @ApiModelProperty(example = "10000", required = true, value = "模板ID")
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public UpdateJobTemplateRequest name(String name) {
+    this.name = name;
+    return this;
+  }
+
+   /**
+   * 模板名称
+   * @return name
+  **/
+  @ApiModelProperty(example = "simple stream sql", value = "模板名称")
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public UpdateJobTemplateRequest desc(String desc) {
+    this.desc = desc;
+    return this;
+  }
+
+   /**
+   * 模板描述
+   * @return desc
+  **/
+  @ApiModelProperty(example = "快速上手示例", value = "模板描述")
+  public String getDesc() {
+    return desc;
+  }
+
+  public void setDesc(String desc) {
+    this.desc = desc;
+  }
+
+  public UpdateJobTemplateRequest sqlBody(String sqlBody) {
+    this.sqlBody = sqlBody;
+    return this;
+  }
+
+   /**
+   * Stream SQL语句, 至少包含source, query, sink三个部分
+   * @return sqlBody
+  **/
+  @ApiModelProperty(example = "select * from source_table", value = "Stream SQL语句, 至少包含source, query, sink三个部分")
+  public String getSqlBody() {
+    return sqlBody;
+  }
+
+  public void setSqlBody(String sqlBody) {
+    this.sqlBody = sqlBody;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    UpdateJobTemplateRequest updateJobTemplateRequest = (UpdateJobTemplateRequest) o;
+    return Objects.equals(this.id, updateJobTemplateRequest.id) &&
+        Objects.equals(this.name, updateJobTemplateRequest.name) &&
+        Objects.equals(this.desc, updateJobTemplateRequest.desc) &&
+        Objects.equals(this.sqlBody, updateJobTemplateRequest.sqlBody);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, name, desc, sqlBody);
+  }
+
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class UpdateJobTemplateRequest {\n");
+    
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    name: ").append(toIndentedString(name)).append("\n");
+    sb.append("    desc: ").append(toIndentedString(desc)).append("\n");
+    sb.append("    sqlBody: ").append(toIndentedString(sqlBody)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+
+}
+
